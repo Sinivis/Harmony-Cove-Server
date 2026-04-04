f7f032c3aba013e2c4499d161952424af6fbf667
Lexen <lexen@Gregnox.lan>
2026-04-04 14:16:11 -0600

    Initial upload, 0

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+# Forge requires a configured set of both JVM and program arguments.
+# Add custom JVM arguments to the user_jvm_args.txt
+# Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
+#  pass them to this script directly
+java -jar packwiz-installer-bootstrap.jar -g -s server https://raw.githubusercontent.com/Sinivis/Harmony-Cove-Modpack/main/Packwiz%20Shard/pack.toml
+java -Xms16G -Xmx16G @user_jvm_args.txt @libraries/net/neoforged/forge/1.20.1-47.1.106/unix_args.txt "$@"
