b9d8ecfbf2478f2572fcc1b62b1afd1faf416375
Lexen <lexen@Gregnox.lan>
2026-04-26 14:15:49 -0600

    Removed Disenchanting Table and Easy Disenchanting; updated mod versions.

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java -jar packwiz-installer-bootstrap.jar -g -s server https://raw.githubusercontent.com/Sinivis/Harmony-Cove-Modpack/main/Packwiz%20Shard/pack.toml
+java -jar packwiz-installer-bootstrap.jar -g -s server https://raw.githubusercontent.com/Sinivis/Harmony-Cove-Modpack/main/pack.toml
 java -Xms16G -Xmx16G @user_jvm_args.txt @libraries/net/neoforged/forge/1.20.1-47.1.106/unix_args.txt "$@"
